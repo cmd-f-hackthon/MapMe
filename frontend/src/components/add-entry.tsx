5885f3ed6e4f6024b0cc2049bcf5b8e7a3e3039a
Eva <125091369+eva820@users.noreply.github.com>
2025-03-08 20:02:27 -0800

    initial look for add-entry popup

--- frontend/src/components/add-entry.tsx
+++ frontend/src/components/add-entry.tsx
@@ -8,46 +8,73 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Input } from "@/components/ui/input"
+import { DatePickerDemo } from "./date-picker"
+import { Input } from "./ui/input"
+import { MoodPicker } from "./mood-picker"
+import { Textarea } from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 
 export function AddEntry() {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
+        <Button variant="outline">Add Memory</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Add Memory</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Track today's memory here. Click save when you're done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
+            <Label className="text-right">
+              Date
+            </Label>
+            <DatePickerDemo/>
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="location-input" className="text-right">
+              Location
             </Label>
             <Input
-              id="name"
-              defaultValue="Pedro Duarte"
+              id="location-input"
+              type="text"
               className="col-span-3"
             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
+            <Label htmlFor="notes-box" className="text-left">
+              Upload Photo
             </Label>
             <Input
-              id="username"
-              defaultValue="@peduarte"
+              id="file-upload"
+              type="file"
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label className="text-right">
+              Mood
+            </Label>
+            <MoodPicker
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="notes-box" className="text-right">
+              Notes
+            </Label>
+            <Textarea 
+              id="notes-box"
+              defaultValue="Write about your day here!"
               className="col-span-3"
             />
           </div>
         </div>
         <DialogFooter>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit">Save Memory</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
